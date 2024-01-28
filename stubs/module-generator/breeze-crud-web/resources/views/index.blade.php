be83cfeb955758c2f235be6e81ea97781c76ee5f
David Carr <dave@dcblog.dev>
2024-01-28 01:58:27 +0000

    styled index table

--- stubs/module-generator/breeze-crud-web/resources/views/index.blade.php
+++ stubs/module-generator/breeze-crud-web/resources/views/index.blade.php
@@ -8,32 +8,49 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="overflow-hidden overflow-x-auto p-6 bg-white border-b border-gray-200">
+                    <div class="min-w-full align-middle">
 
-                    <p><a href="{{ route('{module}.create') }}" class="bg-blue-600 text-white px-1.5 py-2.5 rounded-md my-10">Add {Model}</a></p>
+                        <p><a href="{{ route('{module}.create') }}" class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150 mt-3">Add {Model}</a></p>
 
-                    <table class="mt-10 w-full">
-                        <tr>
-                            <td>Name</td>
-                            <td>Action</td>
-                        </tr>
-                        @foreach(${module} as ${model})
+                        <table class="mt-5 min-w-full divide-y divide-gray-200 border">
+                            <thead>
                             <tr>
-                                <td>{{ ${model}->name }}</td>
-                                <td>
-                                    <a href="{{ route('{module}.edit', ${model}->id) }}" class="text-blue-500">{{ __('Edit') }}</a>
-                                    |
-                                    <a href="{{ route('{module}.destroy', ${model}->id) }}" class="text-blue-500" onclick="event.preventDefault(); if(confirm('Are you sure?')){document.getElementById('delete-book-{{ ${model}->id }}').submit();}">{{ __('Delete') }}</a>
-
-                                    <form action="{{ route('{module}.destroy', ${model}->id) }}" method="POST" id="delete-book-{{ ${model}->id }}" >
-                                        @csrf
-                                        @method('DELETE')
-                                    </form>
-                                </td>
+                                <th class="px-6 py-3 bg-gray-50 text-left">
+                                    <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Name</span>
+                                </th>
+                                <th class="px-6 py-3 bg-gray-50 text-left">
+                                    <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"></span>
+                                </th>
                             </tr>
-                        @endforeach
-                    </table>
+                            </thead>
+
+                            <tbody class="bg-white divide-y divide-gray-200 divide-solid">
+                            @foreach(${module} as ${model})
+                                <tr class="bg-white">
+                                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
+                                        {{ ${model}->name }}
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
+                                        <a href="{{ route('{module}.edit', ${model}->id) }}" class="text-blue-500">{{ __('Edit') }}</a>
+                                        |
+                                        <a href="{{ route('{module}.destroy', ${model}->id) }}" class="text-blue-500" onclick="event.preventDefault(); if(confirm('Are you sure?')){document.getElementById('delete-book-{{ ${model}->id }}').submit();}">{{ __('Delete') }}</a>
+
+                                        <form action="{{ route('{module}.destroy', ${model}->id) }}" method="POST" id="delete-book-{{ ${model}->id }}" >
+                                            @csrf
+                                            @method('DELETE')
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+
+                    <div class="mt-2">
+                        {{ ${module}->links() }}
+                    </div>
 
                 </div>
             </div>
